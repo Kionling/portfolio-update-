11540d98748130d332e51bcb79bf728c09e98ad0
Daniel Jauregui <danieljauregui@Daniels-MacBook-Pro.local>
2023-03-01 09:16:49 -0800

    Updated package.json scripts to run server and heroku build

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,20 @@
-DROP DATABASE IF EXISTS danielDB; 
-CREATE DATABASE danielDB; 
+    DROP DATABASE IF EXISTS danielDB; 
+    CREATE DATABASE danielDB; 
 
-USE danielDB; 
+    USE danielDB; 
 
-CREATE TABLE user (
-    id INTEGER AUTO_INCREMENT,
-    name VARCHAR (20),
-    PRIMARY KEY (id) 
+    CREATE TABLE user (
+        id INTEGER AUTO_INCREMENT,
+        name VARCHAR (20),
+        PRIMARY KEY (id) 
 
-); 
+    ); 
 
-CREATE TABLE email (
-    id INTEGER AUTO_INCREMENT,
-   email VARCHAR (20),
-    PRIMARY KEY (id)
-); 
+    CREATE TABLE email (
+        id INTEGER AUTO_INCREMENT,
+    email VARCHAR (20),
+        PRIMARY KEY (id)
+    ); 
 
 
 

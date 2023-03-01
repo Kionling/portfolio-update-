3833bcc710a36b4087543bbf71fe681fb2475e37
Daniel Jauregui <danieljauregui@Daniels-MacBook-Pro.local>
2023-03-01 09:14:45 -0800

    Updated SQL schemas

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS danielDB; 
+CREATE DATABASE danielDB; 
+
+USE danielDB; 
+
+CREATE TABLE user (
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR (20),
+    PRIMARY KEY (id) 
+
+); 
+
+CREATE TABLE email (
+    id INTEGER AUTO_INCREMENT,
+   email VARCHAR (20),
+    PRIMARY KEY (id)
+); 
+
+
+
+
